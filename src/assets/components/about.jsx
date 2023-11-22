15fdfd4817477773a7496468ed654f07d2acc6c1
Meraj Meraj <devmeraz87@gmail.com>
2023-11-23 00:22:51 +0600

    initial commit for fixing vercel problem with vite__x__react-router-dom

---
+++ src/assets/components/about.jsx
@@ -0,0 +1,16 @@
+const About = () => {
+    
+ 
+    return (
+        <>
+            <div className="about__container">
+                <h1>
+                    About Page &nbsp;
+                    <a href="/">Home</a>
+                </h1>
+            </div>
+        </>
+    );
+}
+ 
+export default About;
